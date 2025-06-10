7a3fdedefcf5354a75335bb407263690b9ebd5d2
abbasmashaddy72 <abbasmashaddy72@gmail.com>
2025-06-10 18:31:44 +0530

    Test: remove obsolete SharedInstallCommandTest
    The test case `SharedInstallCommandTest.php` was checking for conflicting tenant options in the `shared:install` command. This functionality was already implemented and tested in `SharedInstallCommand.php` in a previous commit. Removing the now redundant test simplifies the codebase.

--- tests/SharedInstallCommandTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Symfony\Component\Console\Command\Command;
-
-it('rejects conflicting tenant options', function () {
-    $this->artisan('shared:install', ['--tenant' => true, '--tenant-only' => true])
-        ->expectsOutput('The --tenant and --tenant-only options cannot be used together.')
-        ->assertExitCode(Command::INVALID);
-});
